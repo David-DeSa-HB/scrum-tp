67807f5abee05185189301f672b7589917dcfa8d
FDonovan12 <donovanferreira12@gmail.com>
2024-06-13 16:09:08 +0200

    FindUser and connectionIsCorrect

--- script.js
+++ script.js
@@ -5,21 +5,55 @@ async function loadXMLDoc(filename) {
         let response = await fetch(filename);
         let data = await response.text();
         let parser = new DOMParser();
+
         return parser.parseFromString(data, 'text/xml');
     } catch (error) {
         console.error(error);
     }
 }
 
-function findUser(username, password) {
-    loadXMLDoc('bdd/bss.xml');
+function findUser(xml, username) {
+    const allUsers = xml.querySelectorAll('Utilisateur');
+    allUsers.forEach((user) => {
+        usernameIsGood = user.querySelector('UserName') === username;
+        if (usernameIsGood) {
+            return user;
+        }
+    });
+    return null;
 }
 
-function randomPassword() {}
+function connectionIsCorrect(xml, username, password) {
+    const user = findUser(xml, username);
+    if (user && user.querySelector('Password') === password) {
+        return true;
+    }
+    return false;
+}
 
-function sendMail() {}
+function connectUser(xml, username, password) {
+    if (connectionIsCorrect(xml, username, password)) {
+        //redirect
+        console.log('redirect');
+    }
+    //error
+    console.log('error');
+}
 
-window.addEventListener('load', () => {});
+window.addEventListener('load', () => {
+    username = 'username';
+    password = 'password';
+
+    loadXMLDoc('./Utilisateurs.xml')
+        .then((xml) => connectUser(xml, username, password))
+        .catch(function (error) {
+            console.error(error);
+        });
+});
+
+function generictruc(xmlPRosmise, fonctin) {
+    xmlPRosmise.then(fonction);
+}
 //chercher si l'tilisateur exist
 //on la trouvé (si pas trouvé erreur, sino redirigé)
 //si ok, deuxieme page on récupére ce qu'on sait de lui et on repli les value des fields. il faudra vérifié si tout les champ son rempli (en bonus).
