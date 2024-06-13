9125a399f10bf21ef3bf36ac8c9efc4d2c79c1e4
FDonovan12 <donovanferreira12@gmail.com>
2024-06-13 15:09:06 +0200

    loadxml

--- script.js
+++ script.js
@@ -1,7 +1,25 @@
 function handdleSubmit() {}
 
+async function loadXMLDoc(filename) {
+    try {
+        let response = await fetch(filename);
+        let data = await response.text();
+        let parser = new DOMParser();
+        return parser.parseFromString(data, 'text/xml');
+    } catch (error) {
+        console.error(error);
+    }
+}
+
+function findUser(username, password) {
+    loadXMLDoc('bdd/bss.xml');
+}
+
 function randomPassword() {}
 
 function sendMail() {}
 
 window.addEventListener('load', () => {});
+//chercher si l'tilisateur exist
+//on la trouvé (si pas trouvé erreur, sino redirigé)
+//si ok, deuxieme page on récupére ce qu'on sait de lui et on repli les value des fields. il faudra vérifié si tout les champ son rempli (en bonus).
